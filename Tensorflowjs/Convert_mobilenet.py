43864795d6e6be2d101c20c4f147fc21f70c2491
Shashwat Pandey <39722327+Shaxpy@users.noreply.github.com>
2020-04-11 01:41:47 +0530

    Update Convert_mobilenet.py

--- Tensorflowjs/Convert_mobilenet.py
+++ Tensorflowjs/Convert_mobilenet.py
@@ -2,4 +2,4 @@ from tensorflow import keras
 from tensorflow.keras import backend
 import tensorflowjs as tfjs
 mobileNet = keras.applications.mobilenet.MobileNet()
-tfjs.converters.save_keras_model(mobileNet, '/home/shaxpy/Desktop')
+tfjs.converters.save_keras_model(mobileNet,'/home/shaxpy/Desktop')
